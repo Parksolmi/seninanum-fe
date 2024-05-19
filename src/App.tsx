599a4fa92515205dd8c2393e9f4b3ac010c3e7be
wngus <wngus4595@gmail.com>
2024-05-19 17:37:57 +0900

    chore: save change

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import './styles/reset.css';
-
+import React, { useState } from 'react';
+import StyleButton from './components/common/Button';
 import { Route, Routes } from 'react-router-dom';
 import NavLayout from './layouts/NavLayout';
 
 function App() {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(e.target.checked);
+  };
   return (
     <>
       <h1>시니나눔</h1>
       <div>시니나눔</div>
+      <StyleButton children="다음" varient="dong" />
+
       {/* // <Routes>
     //   <Route element={<NavLayout />}></Route>
     //   //동백 //나리
