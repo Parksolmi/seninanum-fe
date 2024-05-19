2423a35ae9ef7acbd8423a7acb4989ca076486d8
wngus <wngus4595@gmail.com>
2024-05-19 19:16:44 +0900

    feat: add CheckBox

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import './styles/reset.css';
-
+import React, { useState } from 'react';
+import CheckBox from './components/common/CheckBox';
 import { Route, Routes } from 'react-router-dom';
 import NavLayout from './layouts/NavLayout';
 
 function App() {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(e.target.checked);
+  };
   return (
     <>
       <h1>시니나눔</h1>
       <div>시니나눔</div>
+      <CheckBox
+        id="checkbox_labeled"
+        checked={isChecked}
+        onChange={onChange}
+        label="약관에 모두 동의"
+      />
       {/* // <Routes>
     //   <Route element={<NavLayout />}></Route>
     //   //동백 //나리
---
+++ src/components/common/CheckBox.tsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import empty from '../../assets/check-empty.svg';
+import filled from '../../assets/check-filled.svg';
+
+interface CheckboxProps {
+  id: string;
+  checked: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  label?: string | null;
+}
+
+const CheckBox = ({ id, checked, onChange, label }: CheckboxProps) => {
+  return (
+    <CheckBoxContainer>
+      <Checkbox checked={checked} htmlFor={id}>
+        <HiddenCheckbox
+          id={id}
+          type="checkbox"
+          onChange={onChange}
+          checked={checked}
+        />
+        <CheckIcon src={checked ? filled : empty} />
+      </Checkbox>
+      {label ? <Label htmlFor={id}>{label}</Label> : null}
+    </CheckBoxContainer>
+  );
+};
+
+const CheckBoxContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+// checked 상태에 따라 empty/filled.svg 변경
+const Checkbox = styled.label<{ checked: Boolean }>`
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  cursror: pointer;
+  & > svg {
+    ${empty}
+  }
+`;
+const CheckIcon = styled.img`
+  width: 1.875rem;
+  height: 1.875rem;
+`;
+const HiddenCheckbox = styled.input`
+  border: 0;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+`;
+
+const Label = styled.label`
+  display: inline-block;
+  line-height: 16px;
+  cursor: pointer;
+`;
+
+export default CheckBox;
