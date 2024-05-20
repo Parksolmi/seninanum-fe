735a00d4e4f8969cc2adf2ed2e0aa015a94ea4ac
wngus <wngus4595@gmail.com>
2024-05-20 20:08:39 +0900

    feat: 유저타입버튼 구현

---
+++ src/components/signup/UserTypeButton.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import styled from 'styled-components';
+import { types } from 'util';
+
+interface ButtonProps {
+  types: string;
+  isSelected: boolean;
+  onClick: (name: string) => void;
+}
+
+const UserTypeButton: React.FC<ButtonProps> = ({
+  types,
+  isSelected,
+  onClick,
+}: ButtonProps) => {
+  const handleClick = () => {
+    onClick(types);
+  };
+  return (
+    <StyleBoxContainer
+      setType={types}
+      onClick={handleClick}
+      isSelected={isSelected}
+    >
+      <Text1 setType={types}>{types}</Text1>
+      <Text2>
+        {types === '동백'
+          ? '재능을 공유하고 싶어요.'
+          : '재능을 공유받고 싶어요.'}
+      </Text2>
+      <Text3>
+        {types === '동백'
+          ? '은퇴하신 시니어분들 대상이에요.'
+          : '누구나 재능을 공유받을 수 있어요.'}
+      </Text3>
+    </StyleBoxContainer>
+  );
+};
+
+const StyleBoxContainer = styled.div<{ isSelected: boolean; setType: string }>`
+  display: flex;
+  flex-direction: column;
+  width: 20.25rem;
+  height: 10rem;
+  flex-shrink: 0;
+  border-radius: 0.625rem;
+  border: ${({ isSelected, setType }) =>
+    isSelected
+      ? setType === '동백'
+        ? '2px solid rgba(255, 49, 74, 0.7)'
+        : '2px solid rgba(255, 170, 14, 0.70)'
+      : ''};
+  background: ${({ isSelected, setType }) =>
+    isSelected
+      ? setType === '동백'
+        ? 'var(--Secondary-dong-2, #ffedf0)'
+        : 'var(--Secondary-nari-2, #FFF8E3)'
+      : 'var(--Base-White, #fff)'};
+
+  box-shadow: 0px 1px 7.4px 3px rgba(150, 150, 150, 0.25);
+
+  /* &:active {
+    background: var(--Secondary-dong-2, #ffedf0);
+    border: 2px solid rgba(255, 49, 74, 0.7);
+    box-shadow: 0px 1px 5.1px 3px rgba(255, 49, 74, 0.2);
+  } */
+`;
+
+const Text1 = styled.div<{ setType: string }>`
+  display: flex;
+  width: 7.3125rem;
+  height: 1.1875rem;
+  flex-direction: column;
+  justify-content: center;
+  flex-shrink: 0;
+  color: ${({ setType }) =>
+    setType === '동백'
+      ? 'var(--Primary-dong, #ff314a)'
+      : 'var(--Primary-nari, #FFAA0E)'};
+  font-family: 'NanumSquare Neo';
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+const Text2 = styled.div`
+  display: flex;
+  width: 9.75rem;
+  height: 3.375rem;
+  flex-direction: column;
+  justify-content: center;
+  flex-shrink: 0;
+  color: var(--Base-Black, #000);
+  font-family: 'NanumSquare Neo';
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+const Text3 = styled.div`
+  display: flex;
+  width: 16.4375rem;
+  height: 1.75rem;
+  flex-direction: column;
+  justify-content: center;
+  flex-shrink: 0;
+  color: var(--Base-Deep-Gray, #5b5b5b);
+  font-family: 'NanumSquare Neo';
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export default UserTypeButton;
