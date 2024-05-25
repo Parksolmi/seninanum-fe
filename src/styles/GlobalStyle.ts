54b569213f0fc723e928105bfe2254599a56043e
Solmi <7548ck@naver.com>
2024-05-25 15:50:26 +0900

    fix: 폰트 설정 깜박임 현상 해결

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,8 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-import NanumSquareL from '../fonts/NanumSquareL.woff';
-import NanumSquareR from '../fonts/NanumSquareR.woff';
-import NanumSquareB from '../fonts/NanumSquareB.woff';
 
 const theme = {
   colors: {
@@ -16,37 +13,9 @@ const GlobalStyle = createGlobalStyle`
 
   ${reset}
 
-  @font-face {
-    font-family: Nanum_Square;
-    font-style: normal;
-    line-height: 1;
-    src: local('NanumSquareB'), local('NanumSquareB');
-        font-style: normal;
-        src: url(${NanumSquareB}) format('woff');
-    font-display: swap;
-  }
-  @font-face {
-    font-family: Nanum_Square;
-    font-style: normal;
-    line-height: 1;
-    src: local('NanumSquareR'), local('NanumSquareR');
-        font-style: normal;
-        src: url(${NanumSquareR}) format('woff');
-    font-display: swap;
-  }
-  @font-face {
-    font-family: Nanum_Square;
-    font-style: normal;
-    line-height: 1;
-    src: local('NanumSquareL'), local('NanumSquareL');
-        font-style: normal;
-        src: url(${NanumSquareL}) format('woff');
-    font-display: swap;
-  }
-
   *, body {
     box-sizing: border-box;
-    font-family: Nanum_Square;
+    font-family: 'Nanum_Square';
     /* margin: 0 1.1rem; */
   }
 
