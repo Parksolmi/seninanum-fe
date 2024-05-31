4200092223faaea2374aaab9e837520e21e32318
Solmi <7548ck@naver.com>
2024-05-31 14:13:45 +0900

    style: 홈 화면 페이지 스타일 지정

--- src/pages/home/HomeIndexPage.tsx
+++ src/pages/home/HomeIndexPage.tsx
@@ -1,27 +1,24 @@
 import React, { useState } from 'react';
-// import Toggle from '../../components/signup/Toggle';
-import UserTypeButton from '../../components/signin/UserTypeButton';
+import styled from 'styled-components';
 import ApplyMyRecruit from '../../components/home/ApplyMyRecruit';
 import ShortcutButton from '../../components/common/ShortcutButton';
 import RecommendDongCard from './RecommendDongCard';
 import RecommendDongCardDetail from './RecommendDongCardDetail';
 
 const HomeIndexPage: React.FC = () => {
-  const [selectedButton, setSelectedButton] = useState<string | null>(null);
-  const handleButtonClick = (name: string) => {
-    setSelectedButton((prevSelectedButton) =>
-      prevSelectedButton === name ? null : name
-    );
-  };
   return (
-    <>
+    <WrapContent>
       <h1>홈페이지</h1>
       <ApplyMyRecruit />
       <ShortcutButton />
       <RecommendDongCard />
       <RecommendDongCardDetail />
-    </>
+    </WrapContent>
   );
 };
+
+const WrapContent = styled.div`
+  padding: 1.3rem 1.1rem;
+`;
 
 export default HomeIndexPage;
