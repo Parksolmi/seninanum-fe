4ebc154b3bb54bb560e762c1643397aa1d70db4d
Solmi <7548ck@naver.com>
2024-06-03 02:39:41 +0900

    feat: SummaryCard 컴포넌트 구현

--- src/pages/home/HomeIndexPage.tsx
+++ src/pages/home/HomeIndexPage.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import SummaryCard from '../../components/common/SummaryCard';
 import CareerProfileProgress from '../../components/home/CareerProfileProgress';
 
 const HomeIndexPage: React.FC = () => {
   return (
     <WrapContent>
-      <CareerProfileProgress status={2} />
+      <CareerProfileProgress status={0} />
+      <SummaryCard
+        nickname="닉네임"
+        age="20대"
+        method="비대면"
+        content="기후기술 창업대회 공모전 피드백 및 도와주실 전문가 구합니다."
+        fields={['교육', '예능', '디지털']}
+      />
     </WrapContent>
   );
 };
