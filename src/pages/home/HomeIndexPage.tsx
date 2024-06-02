6d9b946b59e88cc219ea81112a61bc7840e261d2
Solmi <7548ck@naver.com>
2024-06-02 21:53:19 +0900

    chore: 안 쓰는 코드 정리

--- src/pages/home/HomeIndexPage.tsx
+++ src/pages/home/HomeIndexPage.tsx
@@ -1,29 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import CareerDetail from '../../components/common/CareerDetail';
-import Category from '../../components/common/Category';
-import ageState from '../../constants/ageState';
 
 const HomeIndexPage: React.FC = () => {
-  const handleDelete = () => {
-    console.log('삭제');
-  };
-
-  return (
-    <WrapContent>
-      <CareerDetail
-        title="IT분야 HR본부 인재관리"
-        period="1993.03 ~ 2023.10"
-        content="세부 업무 내용입니다."
-        onDelete={handleDelete}
-      />
-      <Category
-        label={ageState.label}
-        list={ageState.list}
-        onDelete={handleDelete}
-      />
-    </WrapContent>
-  );
+  return <WrapContent></WrapContent>;
 };
 
 const WrapContent = styled.div`
