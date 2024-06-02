a9c73f6c9b225ce6322ca911187a54fa752ec001
Solmi <7548ck@naver.com>
2024-06-02 20:14:06 +0900

    feat: CareerDetail 컴포넌트 구현

--- src/pages/home/HomeIndexPage.tsx
+++ src/pages/home/HomeIndexPage.tsx
@@ -4,11 +4,21 @@ import ApplyMyRecruit from '../../components/home/MyRecruitProgress';
 import ShortcutButton from '../../components/common/ShortcutButton';
 import RecommendDongCard from '../../components/home/ProfileVerticalCard';
 import RecommendDongCardDetail from '../../components/home/ProfileHorizontalCard';
+import CareerDetail from '../../components/common/CareerDetail';
 
 const HomeIndexPage: React.FC = () => {
+  const handleDelete = () => {
+    console.log('삭제');
+  };
+
   return (
     <WrapContent>
-      <h1>홈페이지</h1>
+      <CareerDetail
+        title="IT분야 HR본부 인재관리"
+        period="1993.03 ~ 2023.10"
+        content="세부 업무 내용입니다."
+        onDelete={handleDelete}
+      />
       <ApplyMyRecruit />
       <ShortcutButton />
       <RecommendDongCard />
