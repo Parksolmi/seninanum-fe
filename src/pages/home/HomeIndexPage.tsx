878743c57613ef4e4ba04ea3008c735ccaaee389
wngus <wngus4595@gmail.com>
2024-06-03 08:22:07 +0900

    feat: 나리 구인글 작성 컴포넌트 구현

--- src/pages/home/HomeIndexPage.tsx
+++ src/pages/home/HomeIndexPage.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import SummaryCard from '../../components/common/SummaryCard';
 import CareerProfileProgress from '../../components/home/CareerProfileProgress';
+import ProgressBar from '../../components/common/ProgressBar';
+import InputPrice from '../../components/common/InputPrice';
 
 const HomeIndexPage: React.FC = () => {
   return (
@@ -15,6 +17,8 @@ const HomeIndexPage: React.FC = () => {
         content="기후기술 창업대회 공모전 피드백 및 도와주실 전문가 구합니다."
         fields={['교육', '예능', '디지털']}
       />
+      <ProgressBar status={2} type={'dong'} />
+      <InputPrice></InputPrice>
     </WrapContent>
   );
 };
