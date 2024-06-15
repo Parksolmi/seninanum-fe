fb9f62a9f6ec2df274f17ecc444fa084b16cf6c0
Solmi <7548ck@naver.com>
2024-06-15 17:14:17 +0900

    fix: 에러 수정

--- src/layouts/NavLayout.tsx
+++ src/layouts/NavLayout.tsx
@@ -2,24 +2,14 @@ import { Outlet } from 'react-router-dom';
 import React from 'react';
 import styled from 'styled-components';
 import TabBar from '../components/common/TabBar';
-import useUserInfo from '../hooks/useUserInfo';
-// import useUserState from '../store/UserState';
 
 const NavLayout = () => {
-  // const { userState } = useUserState();
-  // const userId = userState.userId;
-  const userId = '3518693517'; //test데이터 넣어보기
-
-  const userType = useUserInfo(userId);
-  console.log(userType);
-  if (!userType) return <div>Loading...</div>; // 사용자 타입이 로드될 때까지 로딩 표시
-
   return (
     <>
       <Padding>
-        <Outlet context={{ userType }} />
+        <Outlet context={'dong'} />
       </Padding>
-      <TabBar type={userType} />
+      <TabBar type={'dong'} />
     </>
   );
 };
