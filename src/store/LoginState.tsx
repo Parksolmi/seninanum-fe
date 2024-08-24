aa2f5de67153cdc8f479dfe3351170953e648c0b
Solmi <7548ck@naver.com>
2024-08-24 20:13:14 +0900

    feat: refresh token 구현

--- src/store/LoginState.tsx
+++ src/store/LoginState.tsx
@@ -1,21 +1,28 @@
+import { convertCompilerOptionsFromJson } from 'typescript';
 import { instance } from '../api/instance';
 
-export const login = async (value) => {
+export const login = async (value: any) => {
   try {
     const response = await instance.post('/auth/login', {
       userId: value,
     });
 
     if (response.data.message === 'LOGIN') {
-      const accessToken = response.data.accessToken;
-      const refreshToken = response.data.refreshToken;
-
-      localStorage.setItem('accessToken', accessToken);
-      localStorage.setItem('refreshToken', refreshToken);
+      localStorage.setItem('accessToken', response.data.accessToken);
+      localStorage.setItem('refreshToken', response.data.refreshToken);
     }
 
     return response.data.message;
   } catch (err) {
     console.log(err);
   }
+};
+
+export const refresh = async (value: any) => {
+  const response = await instance.post('/refresh', {
+    refreshToken: value.refreshToken,
+  });
+
+  const { accessToken } = response.data;
+  localStorage.setItem('accessToken', accessToken);
 };
