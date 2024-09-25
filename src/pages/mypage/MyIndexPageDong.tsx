390aeba513a05edd33e2007fef67a762796e5522
Solmi <7548ck@naver.com>
2024-09-25 22:23:36 +0900

    feat: titleHeader 컴포넌트 구현

--- src/pages/mypage/MyIndexPageDong.tsx
+++ src/pages/mypage/MyIndexPageDong.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import TitleHeader from '../../components/header/TitleHeader';
+
 const MyIndexPageDong: React.FC = () => {
-  return <h1>마이페이지-동백</h1>;
+  return <p>동백 내 정보</p>;
 };
 export default MyIndexPageDong;
