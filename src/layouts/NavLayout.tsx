bf999e9fb1b66ec1039f40d232fcec63b23a3b5c
Solmi <7548ck@naver.com>
2024-11-12 12:38:53 +0900

    feat: 비로그인 화면 막기

--- src/layouts/NavLayout.tsx
+++ src/layouts/NavLayout.tsx
@@ -1,11 +1,23 @@
-import { Outlet } from 'react-router-dom';
-import React from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import TabBar from '../components/common/TabBar';
 import { useFetchUserType } from '../hooks/useFetchUserType';
 
 const NavLayout = () => {
   const { data: user } = useFetchUserType();
+
+  // 비로그인시 로그인 페이지로 리다이렉트
+  const navigate = useNavigate();
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    const refreshToken = localStorage.getItem('refreshToken');
+
+    if (!accessToken || !refreshToken) {
+      alert('로그인이 필요합니다!');
+      navigate('/');
+    }
+  }, [navigate]);
 
   return (
     <>
