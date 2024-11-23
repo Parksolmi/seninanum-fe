2c03ac2533af0316658075c5d31188f32c14f324
wngus <wngus4595@gmail.com>
2024-11-24 01:46:45 +0900

    feat:마이페이지 그룹메뉴 리뷰조회,리뷰작성으로 이름변경

--- src/components/mypage/GroupMenu.tsx
+++ src/components/mypage/GroupMenu.tsx
@@ -14,11 +14,11 @@ const GroupMenu: React.FC<GroupMenuProps> = ({ userType }) => {
       <WrapGroupMenu>
         <h3 className="group-title">나의 활동</h3>
         <div className="menu" onClick={() => navigate('/')}>
-          <div>받은 리뷰 조회</div>
+          <div>리뷰 조회</div>
           <img src="/assets/common/arrow-gray.png" alt="계정 관리" />
         </div>
         <div className="menu" onClick={() => navigate('/')}>
-          <div>작성한 리뷰 조회</div>
+          <div>리뷰 작성</div>
           <img src="/assets/common/arrow-gray.png" alt="계정 관리" />
         </div>
         {userType === 'nari' ? (
