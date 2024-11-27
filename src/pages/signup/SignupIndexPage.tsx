732faf30c968a26b35f04ff21a7781a626851cc0
Solmi <7548ck@naver.com>
2024-11-27 21:47:45 +0900

    feat: 관리자 로그인 지움

--- src/pages/signup/SignupIndexPage.tsx
+++ src/pages/signup/SignupIndexPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const SignupIndexPage = () => {
@@ -16,7 +15,6 @@ const SignupIndexPage = () => {
     window.location.href = kakaoURL;
   };
 
-  const navigate = useNavigate();
   return (
     <WrapContent>
       <WrapLogo>
@@ -35,9 +33,9 @@ const SignupIndexPage = () => {
         </div>
       </WrapLogo>
       {/* 관리자 계정 버튼 */}
-      <AdminButton onClick={() => navigate('/signup/admin')}>
+      {/* <AdminButton onClick={() => navigate('/signup/admin')}>
         관리자 계정으로 접속하기
-      </AdminButton>
+      </AdminButton> */}
       <KakaoButton onClick={handleLogin}>
         <img src="/assets/signIn/kakao-icon.png" alt="kakao-logo" />
         카카오톡으로 시작하기
@@ -109,31 +107,31 @@ const KakaoButton = styled.button`
   }
 `;
 
-const AdminButton = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
+// const AdminButton = styled.button`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   gap: 1rem;
 
-  width: 100%;
-  /* border: 4px solid #fff293; */
-  border: none;
-  background-color: #ff314a;
-  border-radius: 0.625rem;
+//   width: 100%;
+//   /* border: 4px solid #fff293; */
+//   border: none;
+//   background-color: #ff314a;
+//   border-radius: 0.625rem;
 
-  height: 3.75rem;
-  font-family: 'NanumSquareR';
+//   height: 3.75rem;
+//   font-family: 'NanumSquareR';
 
-  color: var(--Base-Black, #fff);
-  text-align: center;
-  font-family: NanumSquare;
-  font-size: 1.375rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
+//   color: var(--Base-Black, #fff);
+//   text-align: center;
+//   font-family: NanumSquare;
+//   font-size: 1.375rem;
+//   font-style: normal;
+//   font-weight: 700;
+//   line-height: normal;
 
-  margin-top: 6.25rem;
-  margin-bottom: -4rem;
-`;
+//   margin-top: 6.25rem;
+//   margin-bottom: -4rem;
+// `;
 
 export default SignupIndexPage;
