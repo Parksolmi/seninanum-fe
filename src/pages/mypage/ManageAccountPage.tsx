d103c4e6900ff4ea3ea550fd4978377cbd625d74
wngus <wngus4595@gmail.com>
2024-12-12 04:06:07 +0900

    feat: 회원 탈퇴 프론트

--- src/pages/mypage/ManageAccountPage.tsx
+++ src/pages/mypage/ManageAccountPage.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Modal from '../../components/common/Modal';
 import PrevHeader from '../../components/header/PrevHeader';
 import { useFetchUserType } from '../../hooks/useFetchUserType';
 import useModal from '../../hooks/useModal';
+import { instance } from '../../api/instance';
 
 const ManageAccountPage = () => {
   const navigate = useNavigate();
   const { data: user } = useFetchUserType();
+  const [tel, setTel] = useState('');
 
   //모달창
   const { openModal: openLogoutModal, closeModal: closeLogoutModal } = useModal(
@@ -25,9 +27,35 @@ const ManageAccountPage = () => {
     )
   );
 
+  const { openModal: openDropoutModal, closeModal: closeDropoutModal } =
+    useModal((id) => (
+      <Modal
+        userType={user?.userType || ''}
+        title={'정말 탈퇴하시겠습니까?'}
+        content={'전화번호를 입력해주세요.'}
+        hasInput={true}
+        onChange={(e) => setTel(e.target.value)}
+        cancelText={'취소'}
+        confirmText={'탈퇴하기'}
+        onConfirm={() => handleDropout()}
+        onCancel={closeDropoutModal}
+      />
+    ));
+
   const handleLogout = () => {
     localStorage.clear();
     navigate('/');
+  };
+
+  const handleDropout = async () => {
+    const sanitizedTel = tel.replace(/[^0-9]/g, ''); // 숫자만 추출
+    //console.log('전화번호:', sanitizedTel);
+    try {
+      await instance.post('/dropout', { sanitizedTel });
+      navigate('/');
+    } catch (error) {
+      console.error('회원 탈퇴에 실패했습니다.', error);
+    }
   };
 
   return (
@@ -35,7 +63,7 @@ const ManageAccountPage = () => {
       <PrevHeader title={'계정관리'} navigateTo={'/mypage'} isLine={true} />
       <WrapMenu>
         <div onClick={openLogoutModal}>로그아웃</div>
-        <div>회원탈퇴</div>
+        <div onClick={openDropoutModal}>회원탈퇴</div>
       </WrapMenu>
     </>
   );
