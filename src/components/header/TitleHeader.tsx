9c6ccefc456d6210d4319ad9d7a4e5111a878c54
Solmi <7548ck@naver.com>
2024-12-11 22:58:06 +0900

    feat: 로그아웃 구현

--- src/components/header/TitleHeader.tsx
+++ src/components/header/TitleHeader.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 interface PrevHeaderProps {
@@ -8,18 +7,9 @@ interface PrevHeaderProps {
 }
 
 const TitleHeader = ({ title, isShowAlert }: PrevHeaderProps) => {
-  const navigate = useNavigate();
-
   return (
     <WrapHeader>
       <TitleText>{title}</TitleText>
-      {isShowAlert && (
-        <AlertButton onClick={() => navigate('/arert')}>
-          {' '}
-          {/*알림 페이지 경로 수정하기!!!*/}
-          <img src={'/assets/common/alert.png'} alt="알림페이지" />
-        </AlertButton>
-      )}
     </WrapHeader>
   );
 };
@@ -29,12 +19,6 @@ const WrapHeader = styled.div`
   align-items: center;
   padding: 2.1rem 1.1rem 0.7rem 1.1rem;
   border-bottom: solid 1px #ebeceb;
-`;
-
-const AlertButton = styled.div`
-  img {
-    width: 1.3rem;
-  }
 `;
 
 const TitleText = styled.div`
